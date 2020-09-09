913597172c3c413494da5a7f9a78d4b51ae143d7
leo <leonardo.ncintra@outlook.com>
2020-09-09 15:25:29 -0300

    100% exame controller

--- src/test/java/com/leaolabs/ambrosio/v1/controller/ExameControllerTest.java
+++ src/test/java/com/leaolabs/ambrosio/v1/controller/ExameControllerTest.java
@@ -2,9 +2,14 @@ package com.leaolabs.ambrosio.v1.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leaolabs.ambrosio.model.TipoTemplateExame;
+import com.leaolabs.ambrosio.repository.ExameRepository;
 import com.leaolabs.ambrosio.repository.TipoTemplateExameRepository;
 import com.leaolabs.ambrosio.v1.dtos.ExameDto;
+import lombok.SneakyThrows;
 import org.hamcrest.Matchers;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -32,11 +38,17 @@ public class ExameControllerTest {
     @Autowired
     private TipoTemplateExameRepository tipoTemplateExameRepository;
 
+    @Autowired
+    private ExameRepository exameRepository;
+
     static final String URI = "/v1/ambrosio/exames";
     static final MediaType JSON = MediaType.APPLICATION_JSON;
 
     @Test
-    public void deveRetornar404QuandoNaoExisteNenhumExameCadastrado() throws Exception {
+    @SneakyThrows
+    public void deveRetornar404QuandoNaoExisteNenhumExameCadastrado() {
+        this.exameRepository.deleteAll();
+
         this.mockMvc.perform(get(URI)
                 .contentType(JSON))
                 .andExpect(status().isNotFound())
@@ -47,7 +59,8 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar404QuandoNaoExisteNenhumExameCadastradoById() throws Exception {
+    @SneakyThrows
+    public void deveRetornar404QuandoNaoExisteNenhumExameCadastradoById() {
         this.mockMvc.perform(get(URI + "/83928")
                 .contentType(JSON))
                 .andExpect(status().isNotFound())
@@ -58,7 +71,8 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar400NaBuscaPorIdPassandoStringComoParametro() throws Exception {
+    @SneakyThrows
+    public void deveRetornar400NaBuscaPorIdPassandoStringComoParametro() {
         this.mockMvc.perform(get(URI + "/aaaaa")
                 .contentType(JSON))
                 .andExpect(status().isBadRequest())
@@ -69,7 +83,8 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar400NoCadastroAoNaoPassarADescricao() throws Exception {
+    @SneakyThrows
+    public void deveRetornar400NoCadastroAoNaoPassarADescricao() {
         TipoTemplateExame tipoTemplateExame = getTipoTemplateExameFake();
 
         var exameDto = ExameDto.builder()
@@ -88,7 +103,8 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar400NoCadastroAoPassarADescricaoVazia() throws Exception {
+    @SneakyThrows
+    public void deveRetornar400NoCadastroAoPassarADescricaoVazia() {
         TipoTemplateExame tipoTemplateExame = getTipoTemplateExameFake();
 
         var exameDto = ExameDto.builder()
@@ -108,7 +124,8 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar400NoCadastroAoPassarADescricaoMuitoGrande() throws Exception {
+    @SneakyThrows
+    public void deveRetornar400NoCadastroAoPassarADescricaoMuitoGrande() {
         TipoTemplateExame tipoTemplateExame = getTipoTemplateExameFake();
 
         var exameDto = ExameDto.builder()
@@ -128,7 +145,8 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar400NoCadastroAoNaoPassarOCampoAtivo() throws Exception {
+    @SneakyThrows
+    public void deveRetornar400NoCadastroAoNaoPassarOCampoAtivo() {
         TipoTemplateExame tipoTemplateExame = getTipoTemplateExameFake();
 
         var exameDto = ExameDto.builder()
@@ -147,7 +165,8 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar400NoCadastroAoNaoPassarOTipoDeTemplate() throws Exception {
+    @SneakyThrows
+    public void deveRetornar400NoCadastroAoNaoPassarOTipoDeTemplate() {
         var exameDto = ExameDto.builder()
                 .descricao("Qualquer descricao")
                 .ativo(true)
@@ -164,7 +183,8 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar400NoCadastroComTipoDeTemplateExameSemId() throws Exception {
+    @SneakyThrows
+    public void deveRetornar400NoCadastroComTipoDeTemplateExameSemId() {
         var exameDto = ExameDto.builder()
                 .descricao("Qualquer descricao")
                 .tipoTemplateExame(TipoTemplateExame.builder()
@@ -184,12 +204,13 @@ public class ExameControllerTest {
     }
 
     @Test
-    public void deveRetornar400NoCadastroComTipoDeTemplateExameInexistente() throws Exception {
+    @SneakyThrows
+    public void deveRetornar400NoCadastroComTipoDeTemplateExameInexistente() {
         var exameDto = ExameDto.builder()
                 .descricao("Qualquer descricao")
                 .tipoTemplateExame(TipoTemplateExame.builder()
                         .id(88932L)
-                        .descricao("sem id")
+                        .descricao("Inexistente")
                         .build())
                 .ativo(true)
                 .build();
@@ -202,6 +223,60 @@ public class ExameControllerTest {
                 .andExpect(jsonPath("$.[0].userMessage", Matchers.is("You attempted to get a TipoTemplateExame, but did not find any")))
                 .andReturn()
                 .getResponse();
+    }
+
+    @Test
+    @SneakyThrows
+    public void deveRetornar201NoCadastrodeExame() {
+        getTipoTemplateExameFake();
+
+        var exameDto = ExameDto.builder()
+                .descricao("Qualquer descricao")
+                .tipoTemplateExame(TipoTemplateExame.builder()
+                        .id(1L)
+                        .build())
+                .ativo(true)
+                .build();
+
+        var postExame = this.mockMvc.perform(post(URI)
+                .contentType(JSON)
+                .content(objectMapper.writeValueAsBytes(exameDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.records", hasSize(1)))
+                .andReturn()
+                .getResponse();
+
+        JSONObject obj = new JSONObject(postExame.getContentAsString());
+        JSONArray records = obj.getJSONArray("records");
+        Long idExame = 0L;
+
+        for (int i = 0; i < records.length(); i++) {
+            JSONObject rec = records.getJSONObject(i);
+            idExame = rec.getLong("id");
+        }
+
+        var result = this.exameRepository.findById(idExame);
+
+        Assert.assertTrue(result.isPresent());
+        Assert.assertNotNull(result.get().getUuid());
+        Assert.assertEquals(exameDto.getDescricao(), result.get().getDescricao());
+        Assert.assertTrue(result.get().getAtivo());
+        Assert.assertNotNull(result.get().getDataCriacao());
+        Assert.assertNotNull(result.get().getDataAtualizacao());
+    }
+
+    @Test
+    @SneakyThrows
+    public void deveRetornar200AoBuscarTudosOsExames() {
+        getTipoTemplateExameFake();
+
+        var postExame = this.mockMvc.perform(get(URI)
+                .contentType(JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.records", hasSize(1)))
+                .andReturn()
+                .getResponse();
+
     }
 
     private TipoTemplateExame getTipoTemplateExameFake() {
