acb6fb0e2af3a754c7aac0ad924fdbe0110f8696
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-07-19 14:01:47 -0300

    :bug: fix test rabbitmq

--- src/localidade/localidade.controller.spec.ts
+++ src/localidade/localidade.controller.spec.ts
@@ -3,11 +3,13 @@ import { LocalidadeController } from './localidade.controller';
 import { LocalidadeService } from './localidade.service';
 import { PrismaService } from 'src/prisma.service';
 import { EnderecoService } from 'src/endereco/endereco.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 describe('LocalidadeController', () => {
   let controller: LocalidadeController;
   let prismaService: PrismaService;
   let enderecoService: EnderecoService;
+  let clientRabbit: ClientProxy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,18 +33,26 @@ describe('LocalidadeController', () => {
             update: jest.fn(),
             remove: jest.fn(),
           },
-        }
+        },
+        {
+          provide: 'LOCALIDADES_SERVICE',
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     controller = module.get<LocalidadeController>(LocalidadeController);
     prismaService = module.get<PrismaService>(PrismaService);
     enderecoService = module.get<EnderecoService>(EnderecoService);
+    clientRabbit = module.get<ClientProxy>('LOCALIDADES_SERVICE');
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(prismaService).toBeDefined();
     expect(enderecoService).toBeDefined();
+    expect(clientRabbit).toBeDefined();
   });
 });
--- src/localidade/localidade.service.spec.ts
+++ src/localidade/localidade.service.spec.ts
@@ -2,11 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { LocalidadeService } from './localidade.service';
 import { PrismaService } from 'src/prisma.service';
 import { EnderecoService } from 'src/endereco/endereco.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 describe('LocalidadeService', () => {
   let service: LocalidadeService;
   let prismaService: PrismaService;
   let enderecoService: EnderecoService;
+  let clientRabbit: ClientProxy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,17 +32,25 @@ describe('LocalidadeService', () => {
             remove: jest.fn(),
           },
         },
+        {
+          provide: 'LOCALIDADES_SERVICE',
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     service = module.get<LocalidadeService>(LocalidadeService);
     prismaService = module.get<PrismaService>(PrismaService);
     enderecoService = module.get<EnderecoService>(EnderecoService);
+    clientRabbit = module.get<ClientProxy>('LOCALIDADES_SERVICE');
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(prismaService).toBeDefined();
     expect(enderecoService).toBeDefined();
+    expect(clientRabbit).toBeDefined();
   });
 });
