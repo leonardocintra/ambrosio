2505bf55c77529899421821c60207ca53a59476a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2025-03-17 14:00:12 -0300

    :wrench: githubactions

--- src/configuracoes/diocese/diocese.controller.spec.ts
+++ src/configuracoes/diocese/diocese.controller.spec.ts
@@ -2,16 +2,30 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DioceseController } from './diocese.controller';
 import { DioceseService } from './diocese.service';
 import { PrismaService } from 'src/prisma.service';
+import { LocalidadeService } from 'src/localidade/localidade.service';
+import { TipoLocalidadeService } from '../tipo-localidade/tipo-localidade.service';
+import { TipoDioceseService } from '../tipo-diocese/tipo-diocese.service';
+import { EnderecoService } from 'src/endereco/endereco.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 describe('DioceseController', () => {
   let controller: DioceseController;
   let prismaService: PrismaService;
+  let localidadeService: LocalidadeService;
+  let tipoLocalidadeService: TipoLocalidadeService;
+  let tipoDioceseService: TipoDioceseService;
+  let enderecoService: EnderecoService
+  let clientRabbit: ClientProxy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DioceseController],
       providers: [
         DioceseService,
+        LocalidadeService,
+        TipoLocalidadeService,
+        TipoDioceseService,
+        EnderecoService,
         {
           provide: PrismaService,
           useValue: {
@@ -19,15 +33,32 @@ describe('DioceseController', () => {
             findFirstOrThrow: jest.fn(),
           },
         },
+        {
+          provide: 'LOCALIDADES_SERVICE',
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     controller = module.get<DioceseController>(DioceseController);
     prismaService = module.get<PrismaService>(PrismaService);
+    localidadeService = module.get<LocalidadeService>(LocalidadeService);
+    tipoLocalidadeService = module.get<TipoLocalidadeService>(TipoLocalidadeService);
+    tipoDioceseService = module.get<TipoDioceseService>(TipoDioceseService);
+    enderecoService = module.get<EnderecoService>(EnderecoService);
+    clientRabbit = module.get<ClientProxy>('LOCALIDADES_SERVICE');
+
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(prismaService).toBeDefined();
+    expect(localidadeService).toBeDefined();
+    expect(tipoLocalidadeService).toBeDefined();
+    expect(tipoDioceseService).toBeDefined();
+    expect(enderecoService).toBeDefined();
+    expect(clientRabbit).toBeDefined();
   });
 });
--- src/configuracoes/diocese/diocese.service.spec.ts
+++ src/configuracoes/diocese/diocese.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DioceseService } from './diocese.service';
 import { PrismaService } from 'src/prisma.service';
+import { LocalidadeService } from 'src/localidade/localidade.service';
+import { TipoLocalidadeService } from '../tipo-localidade/tipo-localidade.service';
+import { TipoDioceseService } from '../tipo-diocese/tipo-diocese.service';
+import { EnderecoService } from 'src/endereco/endereco.service';
 
 describe('DioceseService', () => {
   let service: DioceseService;
   let prismaService: PrismaService;
+  let localidadeService: LocalidadeService;
+  let tipoLocalidadeService: TipoLocalidadeService;
+  let tipoDioceseService: TipoDioceseService
+  let enderecoService: EnderecoService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         DioceseService,
+        LocalidadeService,
+        TipoLocalidadeService,
+        TipoDioceseService,
+        EnderecoService,
         {
           provide: PrismaService,
           useValue: {
@@ -17,15 +29,29 @@ describe('DioceseService', () => {
             findFirstOrThrow: jest.fn(),
           },
         },
+        {
+          provide: 'LOCALIDADES_SERVICE',
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     service = module.get<DioceseService>(DioceseService);
     prismaService = module.get<PrismaService>(PrismaService);
+    localidadeService = module.get<LocalidadeService>(LocalidadeService);
+    tipoLocalidadeService = module.get<TipoLocalidadeService>(TipoLocalidadeService);
+    tipoDioceseService = module.get<TipoDioceseService>(TipoDioceseService);
+    enderecoService = module.get<EnderecoService>(EnderecoService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(prismaService).toBeDefined();
+    expect(localidadeService).toBeDefined();
+    expect(tipoLocalidadeService).toBeDefined();
+    expect(tipoDioceseService).toBeDefined();
+    expect(enderecoService).toBeDefined();
   });
 });
