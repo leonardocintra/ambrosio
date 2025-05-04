2fba4a2ee10e900ea76eec4c4853924c6a0e9bde
Leonardo Cintra <leonardo.ncintra@outlook.com>
2025-05-04 19:25:48 -0300

    :recycle: endereco id

--- test/diocese.e2e-spec.ts
+++ test/diocese.e2e-spec.ts
@@ -231,12 +231,12 @@ describe('DioceseController (e2e)', () => {
       });
   });
 
-  it(`/${principal} (PATCH) - 200 | deve atualizar uma diocese com campos válidos`, async () => {
+  it(`/${principal}/:id (PATCH) - 200 | deve atualizar uma diocese com campos válidos`, async () => {
     const dioceseData = {
       descricao: faker.company.name(),
       tipoDiocese: { id: 2, descricao: 'Diocese' },
       endereco: {
-        id: 3,
+        id: 1,
         cep: faker.location.zipCode('########'),
         logradouro: faker.location.streetAddress(),
         numero: '1226',
@@ -247,7 +247,7 @@ describe('DioceseController (e2e)', () => {
     };
 
     const response = await request(app.getHttpServer())
-      .patch(`/${principal}/2`)
+      .patch(`/${principal}/1`)
       .set('Authorization', `Bearer ${token}`)
       .set('Content-Type', 'application/json')
       .send(dioceseData)
