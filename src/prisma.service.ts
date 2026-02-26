f721b5709c72103f5c738018af989ab2aa35f87a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2026-02-26 13:21:34 -0300

    :wrench: remove schema

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,34 +5,13 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 import { PrismaClient } from 'src/prisma/generated-client';
 
-const { DATABASE_URL, DB_SCHEMA, NODE_ENV } = process.env;
-
-if (!DATABASE_URL) {
+if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined.');
 }
 
-const schemaFromUrl = (() => {
-  try {
-    const parsedUrl = new URL(DATABASE_URL);
-    return parsedUrl.searchParams.get('schema') ?? undefined;
-  } catch {
-    return undefined;
-  }
-})();
-
-const shouldForceSchema = NODE_ENV === 'production';
-const targetSchema = shouldForceSchema ? DB_SCHEMA ?? schemaFromUrl : undefined;
-
 const pool = new pg.Pool({
-  connectionString: DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
-
-if (targetSchema) {
-  const safeSchema = targetSchema.replace(/"/g, '');
-  pool.on('connect', (client) => {
-    void client.query(`SET search_path TO "${safeSchema}"`);
-  });
-}
 
 const adapter = new PrismaPg(pool);
 
